99323fe2fd78f5967f90dca769bc8c73f9b24bce
muhriddin20056525 <muhriddindavlatov89@gmail.com>
2025-06-04 09:55:54 +0500

    useTransition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+
 function App() {
-  return <div>App</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        fetch("https://jsonplaceholder.typicode.com/comments")
+          .then((res) => res.json())
+          .then((data) => setData(data));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Search items={data} />
+    </div>
+  );
 }
 
 export default App;
