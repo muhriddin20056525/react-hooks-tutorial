3ce1648ed37bfedc3b29c504a906842ca0e74a8f
muhriddin20056525 <muhriddindavlatov89@gmail.com>
2025-06-05 10:27:45 +0500

    useDeferredValue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import List from "./components/List";
-
 function App() {
   const [input, setInput] = useState("");
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
-
   return (
     <div>
       <input type="text" value={input} onChange={handleChange} />
